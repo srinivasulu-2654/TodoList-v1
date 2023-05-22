4d48a2a8a29e3fd8fc61e7cb77324b8764807789
Srinivasulu <srinuvasulucsv@gmail.com>
2023-05-22 14:50:24 +0530

    Initial Commit

---
+++ date.js
@@ -0,0 +1,41 @@
+
+// console.log(module);
+
+// function getDate() 
+
+// var getDate = function() 
+exports.getDate =  function() {
+
+let today = new Date();
+
+let options = {
+ 
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+
+};
+
+let day = today.toLocaleDateString("en-us",options);
+
+return day;
+}
+
+
+exports.getDay = function () {
+
+    let today = new Date();
+    
+    let options = {
+     
+      weekday: "long",
+    
+    
+    };
+    
+    let day = today.toLocaleDateString("en-us",options);
+    
+    return day;
+    }
+
+   
